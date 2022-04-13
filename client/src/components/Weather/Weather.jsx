c4678eebbe6696e8f7b502f44bd3462bced4f2e4
Racksoup <rack.connor1@gmail.com>
2022-04-13 03:41:54 -0400

    styling weather

--- client/src/components/Weather/Weather.jsx
+++ client/src/components/Weather/Weather.jsx
@@ -7,42 +7,29 @@ import '../../css/utils.css';
 
 const Weather = () => {
   return (
-    <div className='WeatherPage'>
-      <Button>
-        <Link style={{ color: 'white' }} to='/calendar'>
-          Back
-        </Link>
-      </Button>
+    <div className='WeatherPage-Main'>
+      <Link className='Link-Weather' to='/calendar'>
+        <div className='Btn-Weather'>Back</div>
+      </Link>
       <h1 style={{ textAlign: 'center' }}>Weather</h1>
       <div style={{ display: 'flex', justifyContent: 'center', marginTop: '20px' }}>
-        <div
-          style={{
-            display: 'flex',
-            flexDirection: 'column',
-            height: '200px',
-            justifyContent: 'space-between',
-          }}
-        >
-          <Button>
-            <Link style={{ color: 'white' }} to='/daily'>
-              Daily
+        <div className='Weather-NavBox'>
+          <div className='Weather-NavBox-Row'>
+            <Link className='Link-Weather' to='/daily'>
+              <div className='Btn-Weather'>Daily</div>
             </Link>
-          </Button>
-          <Button>
-            <Link style={{ color: 'white' }} to='/forecast'>
-              5 Day Forecast
+            <Link className='Link-Weather' to='/forecast'>
+              <div className='Btn-Weather'>5 Day Forecast</div>
             </Link>
-          </Button>
-          <Button>
-            <Link style={{ color: 'white' }} to='/hourly'>
-              Hourly
+          </div>
+          <div className='Weather-NavBox-Row'>
+            <Link className='Link-Weather' to='/hourly'>
+              <div className='Btn-Weather'>Hourly</div>
             </Link>
-          </Button>
-          <Button>
-            <Link style={{ color: 'white' }} to='/minutely'>
-              Minutely
+            <Link className='Link-Weather' to='/minutely'>
+              <div className='Btn-Weather'>Minutely</div>
             </Link>
-          </Button>
+          </div>
         </div>
       </div>
     </div>
