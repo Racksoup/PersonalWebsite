18d1731e41aa334a333e89c52980505679781c80
Racksoup <rack.connor1@gmail.com>
2022-04-15 14:10:51 -0400

    Created NewList modal

--- client/src/components/Pages/Lists.jsx
+++ client/src/components/Pages/Lists.jsx
@@ -1,20 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../../css/Lists.scss';
 
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
 
+const NewListModal = (props) => {
+  return (
+    <div className='Modal-Background' onClick={() => props.toggleModal(false)}>
+      <div className='Modal' onClick={(e) => e.stopPropagation()}>
+        <h2 className='Modal-Title'>Create New List</h2>
+        <input className='Modal-Input' />
+        <div className='Lists-Btn Modal-Submit'>Submit</div>
+      </div>
+    </div>
+  );
+};
+
 const Lists = () => {
+  const [modal, toggleModal] = useState(false);
+
   return (
     <div className='Lists'>
+      {modal == true && <NewListModal modal={modal} toggleModal={toggleModal} />}
       <div className='Lists-TitleBox'>
         <Link className='Lists-Link' to='/home'>
           <div className='Lists-Btn Lists-BackBtn'>Back</div>
         </Link>
         <h1 className='Lists-Title'>Lists</h1>
       </div>
-      <div className='Lists-Btn Lists-NewList'>
+      <div className='Lists-Btn Lists-NewList' onClick={() => toggleModal(true)}>
         <FontAwesomeIcon className='Lists-NewList-Icon' icon={faPlus} />
         New List
       </div>
