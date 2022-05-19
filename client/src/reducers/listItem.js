d49a01eeff9a58f0135879490088c1f67f3945fa
Racksoup <rack.connor1@gmail.com>
2022-05-19 01:24:15 -0400

    using filter in reducer fucked return value. switched to map

--- client/src/reducers/listItem.js
+++ client/src/reducers/listItem.js
@@ -30,19 +30,15 @@ export default function lists(state = initialState, action) {
         list: state.list.filter((item) => item.title !== payload.title),
       };
     case UPDATED_LISTITEM:
-      let newList = state.list.filter((item) => {
-        if (item._id !== payload._id) {
-          return item;
-        } else {
-          item.title = payload.title;
-          item.checked = payload.checked;
-          return item;
-        }
-      });
-
       return {
         ...state,
-        list: newList,
+        list: state.list.map((item) => {
+          if (item._id !== payload._id) {
+            return item;
+          } else {
+            return payload;
+          }
+        }),
       };
     case DELETED_LIST:
       return {
